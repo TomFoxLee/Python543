62c505290e658c340a6b6f171365a5243693e696
TomFoxLee <lhtlee@gmail.com>
2022-07-06 16:58:29 +0800

    Ch.06

--- PYECourse/Ch.06/e10.4_CalThreeKingdoms.py
+++ PYECourse/Ch.06/e10.4_CalThreeKingdoms.py
@@ -1,7 +1,10 @@
 # Ch.06.6.2 Calculate ThreeKingdoms
 # Find the most 5 used words and counts
+#
 # excludes "将军","却说","荆州","二人","不可","不能","如此"
-# remove duplicate "诸葛亮" or "孔明曰" as "孔明", "关公" or "云长" as "关羽", "玄德" or "玄德曰" as "刘备", "孟德" or "丞相" as "曹操"
+#
+# remove duplicate "诸葛亮" or "孔明曰" as "孔明"; "关公" or "云长" as "关羽";
+# "玄德" or "玄德曰" as "刘备"; "孟德" or "丞相" as "曹操"
 
 import jieba
 
