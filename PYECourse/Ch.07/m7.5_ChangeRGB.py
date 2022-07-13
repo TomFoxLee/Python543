16e6410b83b92aa3beff30f56ba7c628dacd147c
TomFoxLee <lhtlee@gmail.com>
2022-07-13 17:59:46 +0800

    Ch.07

--- PYECourse/Ch.07/m7.5_ChangeRGB.py
+++ PYECourse/Ch.07/m7.5_ChangeRGB.py
@@ -3,7 +3,20 @@
 from PIL import Image
 
 im = Image.open(".\\PYECourse\\Ch.07\\ted.jpg")
-r, g, b = im.split()
 
-om = Image.merge("RGB", (b, g, r))
-om.save(".\\PYECourse\\Ch.07\\tedBGR.jpg")
+print(im.format, im.size, im.mode)
+
+im1 = Image.open(".\\PYECourse\\Ch.07\\ted.jpg")
+im1.thumbnail((96, 96))
+im1.save(".\\PYECourse\\Ch.07\\tedTN.jpg", "JPEG")
+
+im2 = Image.open(".\\PYECourse\\Ch.07\\ted.jpg")
+r, g, b = im2.split()
+
+om2a = Image.merge("RGB", (b, g, r))
+om2a.save(".\\PYECourse\\Ch.07\\tedBGR.jpg")
+
+newg = g.point(lambda i: i * 0.9)
+newb = b.point(lambda i: i < 100)
+om2b = Image.merge(im2.mode, (r, newg, newb))
+om2b.save(".\\PYECourse\\Ch.07\\tedMERGE.jpg")
