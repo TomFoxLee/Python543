05eec288d2de51ffa9b388494a030e9bbb7f31fb
TomFoxLee <lhtlee@gmail.com>
2022-09-26 14:10:46 +0800

    LPY3THW Exercises

---
+++ LPY3THW/ex01.py
@@ -0,0 +1,9 @@
+# Exercise 01 of "Learn Python3 the Hard Way"
+
+print("Hello World!")
+print("Hello Again")
+print("I like typeing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
