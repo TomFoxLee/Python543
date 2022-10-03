88280799c9d352a28f1b2c9e2f76d186efca2599
TomFoxLee <lhtlee@gmail.com>
2022-10-03 18:15:12 +0800

    LPY3THW Exercises

--- LPY3THW/ex17.py
+++ LPY3THW/ex17.py
@@ -2,6 +2,8 @@
 # More Files
 #
 # use exist() to check if file alrady there.
+# "w" overwrite only; "a+" append and read
+# len()  # length of a string
 
 from sys import argv
 from os.path import exists
@@ -13,6 +15,7 @@ print(f"Copying from {from_file} to {to_file}")
 # we could do these two on one line, how?
 in_file = open(from_file)
 indata = in_file.read()
+# indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long.")
 
@@ -20,7 +23,8 @@ print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
-out_file = open(to_file, "w")
+# out_file = open(to_file, "w")
+out_file = open(to_file, "a+")
 out_file.write(indata)
 
 print("Allrihgt, all done.")
