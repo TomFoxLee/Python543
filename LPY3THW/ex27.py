5d5384e2aff7960f9f4c2e435426b3022e13d369
TomFoxLee <lhtlee@gmail.com>
2022-10-10 12:06:08 +0800

    LPY3THW Exercises

---
+++ LPY3THW/ex27.py
@@ -0,0 +1,56 @@
+# Exercise 26 of "Learn Python3 the Hard Way"
+# Memorizing Logic
+#
+# and, or, not
+# != : not equal
+# == : equal
+# >= : great or equal
+# <= : less or equal
+# True, False
+#
+#
+# not
+#   not False   : True
+#   not True    : False
+#
+#
+# or
+#   True or False   : True
+#   True or True    : True
+#   False or True   : True
+#   False or False  : False
+#
+#
+# and
+#   True and False  : False
+#   True and True   : True
+#   False and True  : False
+#   False and False : False
+#
+#
+# not or
+#   not (True or False) : False
+#   not (True or True)  : False
+#   not (False or True) : False
+#   not (False or False): True
+#
+#
+# not and
+#   not (True and False)    : True
+#   not (True and True)     : False
+#   not (False and True)    : Ture
+#   not (False and False)   : True
+#
+#
+# !=
+#   1 != 0  : True
+#   1 != 1  : False
+#   0 != 1  : True
+#   0 != 0  : False
+#
+#
+# ==
+#   1 == 0  : False
+#   1 == 1  : True
+#   0 == 1  : False
+#   0 == 0  : True
