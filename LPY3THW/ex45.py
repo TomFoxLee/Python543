96df9463aa77528fc2492c2f3cf885d8c70a4d35
TomFoxLee <lhtlee@gmail.com>
2022-12-08 16:46:40 +0800

    LPY3THW Exercises

---
+++ LPY3THW/ex45.py
@@ -0,0 +1,23 @@
+# Exercise 45 of "Learn Python3 the Hard Way"
+# You Make a Game
+#
+# Function Style
+#   "funtion" and "method" are the same.
+#   naming function as "command".
+#   keep functions small and simple.
+#
+# Class Style
+#   use “camel case,” as in "SuperGoldFactory".
+#   don't do too much in __init__ function.
+#   other functions should use underscore format, so write "my_awesome_hair".
+#   try not to use variables that come from the module or globals.
+#   always have class Name(object) format
+#
+# Code Style
+#   give your code vertical space so people can read it.
+#   if you can’t read it out loud, it’s probably hard to read.
+#
+# Good Comments
+#   write comments, describe WHY you are doing and WHAT you are doing.
+#   comment "how to use" the function.
+#   keep comments relatively short and to the point.
